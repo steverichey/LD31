a23c91814c017371ae9eb2950d414246fade578b
Steve Richey <srichey@floatlearning.com>
2014-12-06 15:58:40 -0600

    move buttons to own file

---
+++ src/js/button.js
@@ -0,0 +1,66 @@
+/*global GameSprite, TWEEN*/
+
+var GameButton = function(x, y, asset, parent) {
+  GameSprite.call(this, x, y, asset);
+  var self = this;
+  this.interactive = true;
+  this.index = 0;
+  this.parentButtonGroup = parent;
+  var rotatetween = null;
+  var scaletween = null;
+  this.onclicked = function(){};
+  
+  this.mouseover = function(data) {
+    if (rotatetween) rotatetween.stop();
+    rotatetween = new TWEEN.Tween({targetrotation: this.rotation})
+      .to({targetrotation: 0.25}, 250)
+      .easing(TWEEN.Easing.Cubic.InOut)
+      .onUpdate(function() {
+        self.rotation = this.targetrotation;
+      })
+      .start();
+  };
+  
+  this.mouseout = function(data) {
+    if (rotatetween) rotatetween.stop();
+    rotatetween = new TWEEN.Tween({targetrotation: this.rotation})
+      .to({targetrotation: 0}, 250)
+      .easing(TWEEN.Easing.Cubic.InOut)
+      .onUpdate(function() {
+        self.rotation = this.targetrotation;
+      })
+      .start();
+  };
+  
+  this.mousedown = this.touchstart = function(data) {
+    if (scaletween) scaletween.stop();
+    scaletween = new TWEEN.Tween({targetscale: this.scale.x})
+      .to({targetscale: 1.5}, 125)
+      .easing(TWEEN.Easing.Cubic.InOut)
+      .onUpdate(function() {
+        self.scale.set(this.targetscale, this.targetscale);
+      })
+      .start();
+    if (this.onclicked) {
+      if (this.parentButtonGroup && this.parentButtonGroup.blocked) {
+        return;
+      }
+      
+      this.onclicked();
+    }
+  };
+  
+  this.mouseup = this.touchend = this.mouseupoutside = this.touchendoutside = function(data) {
+    if (scaletween) scaletween.stop();
+    scaletween = new TWEEN.Tween({targetscale: this.scale.x})
+      .to({targetscale: 1.0}, 1000)
+      .easing(TWEEN.Easing.Bounce.Out)
+      .onUpdate(function() {
+        self.scale.set(this.targetscale, this.targetscale);
+      })
+      .start();
+  };
+};
+
+GameButton.prototype = Object.create(GameSprite.prototype);
+GameButton.prototype.constructor = GameButton;
