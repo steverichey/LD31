4119cd3f7e9dae905544593436cc3bea76960d4c
Steve Richey <srichey@floatlearning.com>
2014-12-07 15:47:19 -0600

    add options JSON

---
+++ src/js/options.js
@@ -0,0 +1,27 @@
+var gameOptions = [
+  {
+    button  : 'btn-eyes',
+    options : [
+      { button : 'btn-eye-big-blue',
+        asset  : 'contacts-big-blue' },
+      { button : 'btn-eye-big-brown',
+        asset  : 'contacts-big-brown' }
+    ]
+  },
+  {
+    button  : 'btn-mouth',
+    options : []
+  },
+  {
+    button  : 'btn-hair',
+    options : []
+  },
+  {
+    button  : 'btn-makeup',
+    options : []
+  },
+  {
+    button  : 'btn-accessories',
+    options : []
+  }
+];
