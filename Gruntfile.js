155e163a92ef1a2aa71a9643235ea413598f18d6
Steve Richey <srichey@floatlearning.com>
2014-12-07 19:25:51 -0600

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,75 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    browserify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'dest/<%= pkg.name %>.min.js'
-      }
+    concat: {
+        dist: {
+            src: 'src/js/*.js',
+            dest: 'temp/concat.js'
+        }
+    },
+    uglify: {
+        dist: {
+            files: {
+                'temp/min.js': 'temp/concat.js'
+            }
+        }
+    },
+    processhtml: {
+        dist: {
+            files: {
+                'temp/process.html' : ['src/index.html']
+            }
+        }
+    },
+    cssmin: {
+        dist: {
+            files: [{
+                src: ['src/css/style.css'],
+                dest: 'temp/min.css'
+            }]
+        }
+    },
+    copy: {
+        dist: {
+            expand: true,
+            flatten: true,
+            filter: 'isFile',
+            src: ['src/img/**', 'src/snd/**'],
+            dest: 'dist/'
+        }
+    },
+    embed: {
+        options: {
+            threshold: '300KB'
+        },
+        dist: {
+            files: {
+                'temp/embed.html': 'temp/process.html'
+            }
+        }
+    },
+    htmlmin: {
+        dist: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true
+            },
+            files: {
+                'dist/index.html': 'temp/embed.html'
+            }
+        }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-browserify');
-
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-embed');
+  grunt.loadNpmTasks('grunt-processhtml');
+  
   // Default task(s).
-  grunt.registerTask('default', ['browserify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'processhtml',
+                                 'cssmin', 'embed', 'htmlmin', 'copy']);
 };
