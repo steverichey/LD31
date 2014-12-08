e65f2cb16568cd51c1dfac072f885b3e4874c322
Steve Richey <srichey@floatlearning.com>
2014-12-07 18:42:02 -0600

    EYES YOU CAN CHANGE EM

--- src/js/options.js
+++ src/js/options.js
@@ -2,10 +2,26 @@ var gameOptions = [
   {
     button  : 'btn-eyes',
     options : [
+      { button : 'btn-eye-blue',
+        asset  : 'contacts-blue' },
+      { button : 'btn-eye-bluegreen',
+        asset  : 'contacts-blue-green' },
+      { button : 'btn-eye-brown',
+        asset  : 'contacts-brown' },
+      { button : 'btn-eye-green',
+        asset  : 'contacts-green' },
+      { button : 'btn-eye-grey',
+        asset  : 'contacts-gray' },
+      { button : 'btn-eye-red',
+        asset  : 'contacts-red' },
+      { button : 'btn-eye-violet',
+        asset  : 'contacts-violet' },
       { button : 'btn-eye-big-blue',
         asset  : 'contacts-big-blue' },
       { button : 'btn-eye-big-brown',
-        asset  : 'contacts-big-brown' }
+        asset  : 'contacts-big-brown' },
+      { button : 'btn-eye-big-violet',
+        asset  : 'contacts-big-purple' }
     ]
   },
   {
