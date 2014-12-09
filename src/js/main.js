07209bbdb2910f0d05f9797208af047516071ff1
Steve <steverichey@users.noreply.github.com>
2014-12-09 08:54:42 -0600

    fix for safari bug

--- src/js/main.js
+++ src/js/main.js
@@ -150,7 +150,11 @@ function init() {
   }
   
   leftbuttons.setAllOnClicked(function(index) {
-    Snowdio.play('select');
+    try {
+      Snowdio.play('select');
+    } catch (e) {
+      console.log('failed to play select');
+    }
     
     if (index === leftchoice) {
       if (centerbuttons !== null) {
